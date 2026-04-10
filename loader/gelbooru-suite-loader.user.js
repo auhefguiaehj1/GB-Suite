d02ef75518157b73adf4acdc6dca6f4b390c2f65
auhefguiaehj1 <12345678+auhefguiaehj1@users.noreply.github.com>
2026-04-10 18:28:00 +0200

    Fixes auto update link

--- loader/gelbooru-suite-loader.user.js
+++ loader/gelbooru-suite-loader.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name         Gelbooru Suite (Loader)
 // @namespace    anon.suite
-// @version      1.0.30
+// @version      1.0.31
 // @description  Gelbooru Suite - A userscript that adds various enhancements to Gelbooru.
 // @match        https://gelbooru.com/index.php?page=post&s=list*
 // @match        https://gelbooru.com/index.php?page=post&s=view*
 // @grant        none
-// @updateURL    https://raw.githubusercontent.com/auhefguiaehj1/GB-Suite/main/loader.user.js
-// @downloadURL  https://raw.githubusercontent.com/auhefguiaehj1/GB-Suite/main/loader.user.js
+// @updateURL    https://raw.githubusercontent.com/auhefguiaehj1/GB-Suite/main/loader/gelbooru-suite-loader.user.js
+// @downloadURL  https://raw.githubusercontent.com/auhefguiaehj1/GB-Suite/main/loader/gelbooru-suite-loader.user.js
 
 // @require      https://raw.githubusercontent.com/auhefguiaehj1/GB-Suite/main/mods/gb.core.js
 
